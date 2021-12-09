4275dab04dc10d0e53161594e4e49eddd36e0107
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-09 12:20:54 +0100

    set all numeric buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,4 +12,31 @@ public class Controller {
     public void setButtonOne(){
         textField.setText(textField.getText() + buttonOne.getText());
     }
+    public void setButtonTwo(){
+        textField.setText(textField.getText() + buttonTwo.getText());
+    }
+    public void setButtonThree(){
+        textField.setText(textField.getText() + buttonThree.getText());
+    }
+    public void setButtonFour(){
+        textField.setText(textField.getText() + buttonFour.getText());
+    }
+    public void setButtonFive(){
+        textField.setText(textField.getText() + buttonFive.getText());
+    }
+    public void setButtonSix(){
+        textField.setText(textField.getText() + buttonSix.getText());
+    }
+    public void setButtonSeven(){
+        textField.setText(textField.getText() + buttonSeven.getText());
+    }
+    public void setButtonEight(){
+        textField.setText(textField.getText() + buttonEight.getText());
+    }
+    public void setButtonNine(){
+        textField.setText(textField.getText() + buttonNine.getText());
+    }
+    public void setButtonZero(){
+        textField.setText(textField.getText() + buttonZero.getText());
+    }
 }
