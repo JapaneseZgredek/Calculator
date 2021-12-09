14b0705248395a5ba40c81d05f938b38fcb08ba7
Fabian Zbrański <fabian.zbranski@gmail.com>
2021-12-09 12:16:12 +0100

    set button one

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField textField;
+    @FXML
+    private Button buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonSeven, buttonEight, buttonNine, buttonZero, buttonDot, buttonEqual, buttonPlus, buttonMinus, buttonTimes, buttonDivide;
+    public void setButtonOne(){
+        textField.setText(textField.getText() + buttonOne.getText());
+    }
 }
